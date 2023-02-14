34827524603dddfec80836f11d0985bb8b7859ec
Σa§t£r <85113072+Ma3t3r@users.noreply.github.com>
2023-02-14 10:01:54 -0300

    Update csv_difference_report_generator.py

--- comparative_scripts/csv_difference_report_generator.py
+++ comparative_scripts/csv_difference_report_generator.py
@@ -1,7 +1,34 @@
 import csv
+import openpyxl
+import os
+
+def xlsx_to_csv(file):
+    # load the .xlsx file as a workbook
+    workbook = openpyxl.load_workbook(file)
+
+    # for each sheet, save as a separate .csv file
+    for sheet in workbook:
+        sheet_name = sheet.title
+        csv_file = f"{sheet_name}.csv"
+
+        with open(csv_file, 'w', newline='') as f:
+            csv_writer = csv.writer(f)
+
+            for row in sheet.iter_rows():
+                row_values = [cell.value for cell in row]
+                csv_writer.writerow(row_values)
+
+        # return the name of the converted .csv file
+        return csv_file
 
 def compare_csv(file1, file2):
-    # carrega os dados dos arquivos CSV em dicionários
+    # check if either file is .xlsx and convert it to .csv if so
+    if file1.endswith('.xlsx'):
+        file1 = xlsx_to_csv(file1)
+    if file2.endswith('.xlsx'):
+        file2 = xlsx_to_csv(file2)
+
+    # load the data from the CSV files into dictionaries
     with open(file1, 'r') as f1:
         reader1 = csv.DictReader(f1)
         data1 = [row for row in reader1]
@@ -10,7 +37,7 @@ def compare_csv(file1, file2):
         reader2 = csv.DictReader(f2)
         data2 = [row for row in reader2]
 
-    # encontra as diferenças entre os dois arquivos CSV
+    # find the differences between the two CSV files
     differences = []
     for row1 in data1:
         found = False
@@ -21,17 +48,23 @@ def compare_csv(file1, file2):
         if not found:
             differences.append(row1)
             
-    # escreve as diferenças em um novo arquivo CSV
+    # write the differences to a new CSV file
     with open('differences.csv', 'w') as f:
         writer = csv.DictWriter(f, fieldnames=reader1.fieldnames)
         writer.writeheader()
         for row in differences:
             writer.writerow(row)
     
-    # gera um relatório com as diferenças
-    report = f"Foram encontradas {len(differences)} diferenças entre os arquivos {file1} e {file2}."
+    # generate a report with the differences
+    report = f"Found {len(differences)} differences between the files {file1} and {file2}."
     print(report)
+    
+    # count the number of values in the "IP Address" column of both files
+    count1 = sum(1 for row in data1 if row.get("IP Address"))
+    count2 = sum(1 for row in data2 if row.get("IP Address"))
+    print(f"File {file1} has {count1} values in the 'IP Address' column.")
+    print(f"File {file2} has {count2} values in the 'IP Address' column.")
 
-file1 = 'file1.csv'
+file1 = 'file1.xlsx'
 file2 = 'file2.csv'
 compare_csv(file1, file2)
