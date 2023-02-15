615198bd66d6f2d30fc5ee3d008a584f0df297f5
Σa§t£r <85113072+Ma3t3r@users.noreply.github.com>
2023-02-15 14:16:35 -0300

    Update Geral_linux.py

--- Linux_Log Analysis/Geral_linux.py
+++ Linux_Log Analysis/Geral_linux.py
@@ -5,17 +5,16 @@ def parse_log(logfile):
     suspicious_activities = []
     with open(logfile, 'r') as f:
         logs = f.readlines()
-        for log in logs:
+        for i, log in enumerate(logs):
             if 'Failed password' in log:
-                suspicious_activities.append('Failed password attempt')
+                suspicious_activities.append(('Failed password attempt', i+1, logfile))
             elif 'Invalid user' in log:
-                suspicious_activities.append('Invalid user attempt')
+                suspicious_activities.append(('Invalid user attempt', i+1, logfile))
             elif 'Accepted password' in log and 'for illegal user' in log:
-                suspicious_activities.append('Accepted password for illegal user')
+                suspicious_activities.append(('Accepted password for illegal user', i+1, logfile))
             elif 'reverse mapping' in log and 'checking getaddrinfo' in log:
-                suspicious_activities.append('Reverse mapping checking getaddrinfo for server')
+                suspicious_activities.append(('Reverse mapping checking getaddrinfo for server', i+1, logfile))
     return suspicious_activities
-
 def main():
     logs_path = '/var/log/'
     log_files = [f for f in os.listdir(logs_path) if f.endswith('.log')]
@@ -25,8 +24,8 @@ def main():
         suspicious_activities += activities
     if suspicious_activities:
         print('Found suspicious activities:')
-        for activity in set(suspicious_activities):
-            print('- ' + activity)
+        for activity, line_num, file_name in set(suspicious_activities):
+            print('- ' + activity + ' in line ' + str(line_num) + ' of ' + file_name)
     else:
         print('No suspicious activities found.')
 
